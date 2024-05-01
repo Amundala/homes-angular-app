4de97c621e238fc45196acf54579cce3e1e9328e
Amundala <tom.amundala@a2k.africa>
2024-05-01 12:47:02 +0300

    logo home link, and cleans up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './components/test/test.component';
-import { HomeComponent } from './components/home/home.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, HomeComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
