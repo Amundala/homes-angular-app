8c5007bcb2a666373127f9a78cedcfedd124072c
Amundala <tom.amundala@a2k.africa>
2024-05-01 12:40:09 +0300

    Navigation to specific house details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from '../../interfaces/housing-location';
+import { HousingService } from '../../services/housing.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  houseLocationId = -1;
+  housingLocationDetail: HousingLocation | undefined;
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.houseLocationId = Number(this.route.snapshot.params['id']);
 
+    this.housingLocationDetail = this.housingService.getHousingLocationById(
+      this.houseLocationId
+    );
+  }
 }
